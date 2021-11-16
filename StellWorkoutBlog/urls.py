b54d77c6927a71ae89b0e03f0cd75b25cb5cf5b7
AngeloZan <angelo_zanonaneto@outlook.com>
2021-11-15 22:25:00 -0300

    Alterando o título da página de administração

--- StellWorkoutBlog/urls.py
+++ StellWorkoutBlog/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+# mudando o titulo da pagina admin
+admin.site.site_header = 'Administração StellWorkout'
+
 from pages.views import (
     home_view,
 )
