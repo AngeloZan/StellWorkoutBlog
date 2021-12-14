ef2118b415f5b5ebe4898e5714624959a7d737a6
AngeloZan <angelo_zanonaneto@outlook.com>
2021-12-14 18:27:19 -0300

    Começando mudança no front da página de posts (owl carousel)

---
+++ blog/static/blog/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    items:8,
+    loop:false,
+    merge:true,
+    margin:30,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            margin:15,
+            items:4,
+            nav:true
+        },
+        600:{
+            margin:20,
+            items:6,
+            nav:true
+        },
+        1000:{
+            items:8,
+            nav:true,
+            mergeFit:false
+        }
+    }
+})
+
