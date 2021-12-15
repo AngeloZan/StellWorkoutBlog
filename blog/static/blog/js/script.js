864975af52f6bc01a1b2a29a8ca57b7cfb8ed5e0
AngeloZan <angelo_zanonaneto@outlook.com>
2021-12-15 00:08:55 -0300

    Opacity gradient (owl carousel)

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -5,27 +5,29 @@ $(document).ready(function(){
 $('.owl-carousel').owlCarousel({
     items:8,
     loop:false,
+    nav:false,
     merge:true,
     margin:30,
+    transitionStyle: "fade",
+    animateIn:'fadeIn',
+    animateOut:'fadeOut',
     responsiveClass:true,
     responsive:{
         0:{
             margin:15,
             items:4,
-            nav:true
         },
         600:{
             margin:20,
             items:6,
-            nav:true
         },
         1000:{
             items:8,
-            nav:true,
             mergeFit:false
         }
     }
 })
+
 
 
 
