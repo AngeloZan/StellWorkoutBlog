e4cf71eb11bc84bc8ffc521e05272e9db039652f
AngeloZan <angelo_zanonaneto@outlook.com>
2022-01-30 11:42:42 -0300

    Update settings.py

--- StellWorkoutBlog/settings.py
+++ StellWorkoutBlog/settings.py
@@ -150,10 +150,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 # Email stuff
 TEST_EMAIL = False
 
